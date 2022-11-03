73c6444cc827d414c95f60f8849643ed4a67356a
Walther Chen <walther.chen@gmail.com>
2022-11-02 23:29:03 -0400

    start grph

--- build.zig
+++ build.zig
@@ -16,7 +16,20 @@ pub fn build(b: *std.build.Builder) void {
     // between Debug, ReleaseSafe, ReleaseFast, and ReleaseSmall.
     const mode = b.standardReleaseOptions();
 
-    const problems = [_][]const u8{ "ini", "dna", "rna", "revc", "fib", "gc", "iprb", "prot", "subs", "cons", "fibd" };
+    const problems = [_][]const u8{
+        "ini",
+        "dna",
+        "rna",
+        "revc",
+        "fib",
+        "gc",
+        "iprb",
+        "prot",
+        "subs",
+        "cons",
+        "fibd",
+        "grph",
+    };
 
     inline for (problems) |problem| {
         const exe = b.addExecutable(problem, "src/" ++ problem ++ ".zig");
---
+++ src/grph.zig
@@ -0,0 +1,11 @@
+const std = @import("std");
+const util = @import("util.zig");
+const FastaCollection = util.FastaCollection;
+const data = @embedFile("../input/grph.txt");
+
+pub fn main() !void {
+    var fasta_collection = FastaCollection.from_str(data);
+    while (try fasta_collection.next()) |*fasta| {
+        std.debug.print("processing: {s}\n", .{fasta.label});
+    }
+}
